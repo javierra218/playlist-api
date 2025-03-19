36b336224de9a7570f9743aef40eaf87009ccf57
Javier Rojas <javierra@unicauca.edu.co>
2025-03-19 18:55:43 -0500

    feat: agregar soporte CORS en la configuración de seguridad y controladores

--- src/main/java/com/quipux/playlist/controller/AuthController.java
+++ src/main/java/com/quipux/playlist/controller/AuthController.java
@@ -5,12 +5,14 @@ import com.quipux.playlist.security.JwtUtils;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 public class AuthController {
 
